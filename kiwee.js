54485c33c2d023f8b608f9948d3efa44c31d0485
Joanna Ostrowska <ostrowska.joasia@gmail.com>
2018-03-15 00:27:40 +0100

    fully functional, not sure if tags should be counted as characters

--- kiwee.js
+++ kiwee.js
@@ -1,9 +1,8 @@
 //this script counts also signs in <...>
 //this solution assumes that signs < and > occur only as html tags
 
-s = '<p class="paragraph">Turnip greens yarrow ricebean rutabaga endive cauliflower sea  lettuce kohlrabi amaranth water <a href="https://www.google.pl/search?q=spinach" class="link">spinach avocado daikon Skartoffel napa cabbage <c>asparagus winter purslane kale. Celery potato scallion desert</c> raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize <span style="font-size: 19px;color: blue;">bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea.</span> Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.</p>';
+s = '<p class="paragraph">Turnip greens yarrow ricebean rutabaga endive cauliflower sea  lettuce kohlrabi amaranth water <a href="https://www.google.pl/search?q=spinach" class="link">spinach avocado daikon Skartoffel napa cabbage <strong>asparagus winter purslane kale.</strong> Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize <span style="font-size: 19px;color: blue;">bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea.</span> Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.</p>';
 
-r = 'Turnip greens yarrow ricebean rutabaga endive cauliflower sea  lettuce kohlrabi amaranth water';
 
 console.log(s);
 
@@ -16,12 +15,11 @@ function openTagSeek(k, n){
             var m = openTagSeek(l, n-b);
             if(m===l || m.endsWith('>')===true){
                 var x;
-                if(k.substring(a,b+1).indexOf(' ') < k.substring(a,b+1).indexOf('>')){
-                    x = k.substring(a,b+1).indexOf(' ');
+                if(k.substring(a,b+1).indexOf(' ')!==-1){
+                    x = a + k.substring(a,b+1).indexOf(' ');
                 }else{
-                    x = k.substring(a,b+1).indexOf('>');
+                    x = b;
                 }
-                console.log('-----'+k.substring(a, x)+'-----------');
                 var p = closeTagSeek(m, k.substring(a+1, x));
                 return k.substring(0, b+1) + p;
             };
@@ -53,9 +51,9 @@ function fullWord(s,n){
         return s;
     }
 }
-
-var n = 310;
-
+console.log('------------------------------------------------------');
+var n = 380;
 var m = fullWord(s,n-3);
 var j = openTagSeek(m, n-3);
-console.log(j);
+var final = j + '...';
+console.log(final);
